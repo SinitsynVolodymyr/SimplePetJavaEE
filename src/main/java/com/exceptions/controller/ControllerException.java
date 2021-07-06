8f6d44cd955df4612f7ecb70d052fafe984ae873
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-07-06 15:58:05 +0300

    create user ControllerException

---
+++ src/main/java/com/exceptions/controller/ControllerException.java
@@ -0,0 +1,5 @@
+package com.exceptions.controller;
+
+public abstract class ControllerException extends Exception{
+
+}
---
+++ src/main/java/com/exceptions/controller/UserIsExistException.java
@@ -0,0 +1,4 @@
+package com.exceptions.controller;
+
+public class UserIsExistException extends ControllerException{
+}
